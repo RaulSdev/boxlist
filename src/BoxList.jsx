b9033c18bf48d14764727c0392c7df4ae5dd3c03
Raul <raulsdev@gamil.com>
2021-09-21 13:04:38 -0500

    started project

---
+++ src/BoxList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BoxList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Color box maker thingy</h1>
+      </div>
+    );
+  }
+}
